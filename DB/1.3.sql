8d27ce5679adbc754bb98b0e49bb0a923709d432
EliseRybka <35104057+EliseRybka@users.noreply.github.com>
2018-01-05 13:03:33 +0300

    Delete 1.3.sql

--- DB/1.3.sql
+++
@@ -1,9 +0,0 @@
-SELECT 'SP TENNIS RACKET' AS Product, SUM((Actual_Price - Min_Price) * Quantity) AS Profit
-
-FROM ((Item INNER JOIN Sales_order ON Item.Order_ID = Sales_order.Order_ID) 
-INNER JOIN Product ON Item.Product_ID = Product.Product_ID) 
-INNER JOIN Price ON ((Item.Product_ID = Price.Product_ID) AND ((Ship_Date BETWEEN Start_Date AND End_Date) OR (Ship_Date > Start_Date AND End_Date IS 
-NULL)))
-
-WHERE Description = 'SP TENNIS RACKET'
-AND Actual_Price > Min_Price
